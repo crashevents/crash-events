6cde6f5d63c47e3b6743230d1114074caad667e5
David Davis <david@wippoc.com>
2014-10-25 20:06:50 -0700

    bootstrap:: reimplement via ember-cli-bootstrap-sass
    problem: /assets/bootstrap-theme.css.map was throwing a 404 (Not Found)
             error
    
    per docs at https://www.npmjs.org/package/ember-cli-bootstrap-sass:
     * npm install --save-dev ember-cli-bootstrap-sass
    
    849d07ff16ab2f76d207bd3becceaf19aaa415bb might have been an outdated way
    to integrate bootstrap.
     * $ bower uninstall --save bootstrap
    
    6cfd7aa8aeb507949bd58b41fa147f486b9ac3d5 is replaced with directives in
    app/styles/app.scss

--- Brocfile.js
+++ Brocfile.js
@@ -4,11 +4,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-app.import('bower_components/bootstrap/dist/css/bootstrap.css.map');
-app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
-app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css.map');
-
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
